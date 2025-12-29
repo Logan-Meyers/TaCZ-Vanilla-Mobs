f72d3192e4d9d15ecd1e44df8bd66f2167e600ed
Logan Meyers <theterrarian03@gmail.com>
2025-12-28 22:52:28 -0800

    added template fabric files

--- README.md
+++
@@ -1,2 +0,0 @@
-# TaCZ-Vanilla-Mobs
-Simple Fabric mod which adds a chance to spawn with weapons from TaCZ: Refabricated
---
+++ src/main/java/net/spud03/tacz_vanilla_mobs/TaCZVanillaMobsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.spud03.tacz_vanilla_mobs;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TaCZVanillaMobsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
